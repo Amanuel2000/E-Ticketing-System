1af60f49cb7a5ee9956863171bde9fddf05ff404
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-24 03:29:40 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,43 @@
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },{
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/register",
+      element: <Register />
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    },
+    {
+      path: "/profile",
+      element: <Profile />
+    },
+  ]);
   return (
     <div>
-      <App />
+      <RouterProvider router={router} />
     </div>
   );
 }

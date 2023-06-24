f80ad6b6d39ade52ceb6e232a34d16459eccf708
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-24 03:14:40 -0700

    Frontend: login and registration page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,31 @@
-import './login.scss'
+import { Link } from "react-router-dom";
+import "./login.scss";
 
-export default function Login() {
+const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome</h1>
+          <p>
+            Watch all Ethiopian Premier League football matches. Buy tickets online and Enjoy the game.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,33 @@
-import './register.scss'
+import { Link } from "react-router-dom";
+import "./register.scss";
 
-export default function Register() {
+const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome</h1>
+          <p>
+            Watch all Ethiopian Premier League football matches. Buy tickets online and Enjoy the game.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;

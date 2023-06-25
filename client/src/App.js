6c2531f6188bd61b43dc9728bdb452e16be3adf5
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-25 13:15:30 -0700

    Update navbar and app.js for setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,71 @@
-function App() {
-  return (
-    <div>
-      <App />
-    </div>
-  );
-}
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
+import Fixtures from "./pages/fixtures/Fixtures";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 
+function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    );
+  };
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/fixtures",
+          element: <Fixtures />
+        },
+        {
+          path: "/about",
+          element: <About />
+        },
+        {
+          path: "/contact",
+          element: <Contact />
+        },
+        {
+          path: "/profile",
+          element: <Profile />
+        },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/register",
+      element: <Register />
+    }
+  ]);
+  return (
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    );
+}
 export default App;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,42 +1,34 @@
 import { SearchOutlined } from "@mui/icons-material";
 import "./navbar.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="left">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span>E-ticket</span>
-        </Link>
+        <div>
+          <NavLink to="/" className="nav-link">E-ticket</NavLink>
+        </div>
       </div>
       <div className="center">
         <div className="search">
           <input type="text" placeholder="Search football match..." />
-          <SearchOutlined style={{ cursor: "pointer", fontSize: "35px"}}/>
+          <SearchOutlined style={{ cursor: "pointer" }}/>
         </div>
       </div>
       <div className="right">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span>Home</span>
-        </Link>
-        <Link to="/fixtures" style={{ textDecoration: "none" }}>
-          <span>Fixtures</span>
-        </Link>
-        <Link to="/about" style={{ textDecoration: "none" }}>
-          <span>About</span>
-        </Link>
-        <Link to="/contact" style={{ textDecoration: "none" }}>
-          <span>Contact Us</span>
-        </Link>
-        <div className="user">
-          <img
+        <div className="nav">
+          <NavLink to="/" className="nav-link">Home</NavLink>
+          <NavLink to="fixtures" className="nav-link">Fixtures</NavLink>
+          <NavLink to="about" className="nav-link">About Us</NavLink>
+          <NavLink to="contact" className="nav-link">Contact Us</NavLink>
+          <NavLink to="profile" className="nav-link">
+            <img
             src="https://images.pexels.com/photos/322819/pexels-photo-322819.jpeg?auto=compress&cs=tinysrgb&w=1600"
             alt=""
-          />
-          <Link to="/profile" style={{ textDecoration: "none" }}>
-            <span>Aman F.</span>
-          </Link>
+            />
+            Aman F.
+          </NavLink>
         </div>
       </div>
     </div>

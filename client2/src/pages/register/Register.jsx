6382e2870cb59120dcec7174155402a9f3f667a4
Elias-Dewa <elias.dewa19@gmail.com>
2023-07-19 02:52:50 -0700

    form custom validation

--- client2/src/pages/register/Register.jsx
+++ client2/src/pages/register/Register.jsx
@@ -5,8 +5,26 @@ import axios from 'axios';
 import './register.css'
 import { Lock, Mail, Person } from '@material-ui/icons'
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from 'yup';
+
+const schema = yup.object({
+  username: yup.string().required("Username is required").min(4, "Username must at least 4 characters"),
+  email: yup.string().required("Email is required").email("Enter a valid email address"),
+  password: yup.string().required("Password is required").min(8, "Password must at least 8 characters"),
+  confirm_password: yup.string().required("Confirmation is required").oneOf([yup.ref("password")], "Password must be match")
+})
 
 const Register = () => {
+  // Form validation
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+  console.log(errors);
 
   const [inputs, setInputs] = useState({
     username: "",
@@ -35,59 +53,64 @@ const Register = () => {
   return (
     <div>
       <div className="register">
-        <div className="register-card">
-          <h1>Register</h1>
-          <form>
-            <div>
-              <Person style={{ color: "#21465b" }}/>
-              <input
-                type="text"
-                name="username"
-                placeholder="Username"
-                onChange={handleChange}
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="input-group">
+            <Person style={{ color: "#21465b" }}/>
+            <input
+              type="text"
+              {...register("username")}
+              placeholder="Username"
+              autoComplete="off"
+              onChange={handleChange}
+              // {required: "Username is required"}
               />
-            </div>
+          </div>
+          <p style={{color:"red", fontSize:"0.9rem", marginTop:"0"}}>{errors.username?.message}</p>
 
-            <div>
-              <Mail style={{ color: "#21465b" }}/>
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                onChange={handleChange}
-              />
-            </div>
+          <div className="input-group">
+            <Mail style={{ color: "#21465b" }}/>
+            <input
+              type="email"
+              {...register("email")}
+              placeholder="Email"
+              onChange={handleChange}
+            />
+          </div>
+          <p style={{color:"red"}}>{errors.email?.message}</p>
 
-            <div>
-              <Lock style={{ color: "#21465b" }}/>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={handleChange}
-              />
-            </div>
+          <div className="input-group">
+            <Lock style={{ color: "#21465b" }}/>
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Password"
+              onChange={handleChange}
+            />
+          </div>
+          <p style={{color:"red"}}>{errors.password?.message}</p>
 
-            <div>
-              <Lock style={{ color: "#21465b" }}/>
-              <input
-                type="password"
-                name="confirm_password"
-                placeholder="Confirm Password"
-                onChange={handleChange}
-              />
-            </div>
+          <div className="input-group">
+            <Lock style={{ color: "#21465b" }}/>
+            <input
+              type="password"
+              {...register("confirm_password")}
+              placeholder="Confirm Password"
+              onChange={handleChange}
+            />
+          </div>
+          <p style={{color:"red"}}>{errors.confirm_password?.message}</p>
 
-            {err && err }
-            <button onClick={handleClick}>Register</button>
-          </form>
+          {err && err }
+          {/* <button>Sign in</button> */}
+          <button onClick={handleClick}>Register</button>
+        </form>
 
-          <p>
-            Do you have an account?
-            <Link to="/login" style={{cursor: "pointer", fontSize:"1.2rem", fontWeight:"600", color:"blue"}}> <span>Login</span>
-            </Link>
-          </p>
-        </div>
+        <p>
+          Do you have an account?
+          <Link to="/login" style={{cursor: "pointer", fontSize:"1.2rem", fontWeight:"600", color:"blue"}}> <span>Login</span>
+          </Link>
+        </p>
       </div>
     </div>
   )
